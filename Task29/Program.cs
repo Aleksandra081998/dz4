396d02ff8a63191912da73f34c11368f4d635ea1
Alexandra <sashulya_dolgova@mail.ru>
2022-08-18 00:06:59 +0700

    dz

---
+++ Task29/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача 29. Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+Console.Clear();
+Console.WriteLine("Ниже ваш массив из 4 случайных чисел");
+Random rnd=new Random();
+int[] array=new int[4];
+for (int i=0;i<array.Length;i++)
+{
+    array[i]=rnd.Next();
+    Console.WriteLine(array[i]); 
+}
